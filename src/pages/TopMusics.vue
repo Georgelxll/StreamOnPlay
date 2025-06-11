e9c67725f1dc5d4ec173248da9239e996e038095
George Custódio Dellangélica Tomaz <george.tomaz@hprodomain.com.br>
2025-06-11 08:42:20 -0300

    testando adição de cards

--- src/pages/TopMusics.vue
+++ src/pages/TopMusics.vue
@@ -3,22 +3,67 @@
     <v-main>
       <!-- Conteúdo da página TopMusics -->
       <v-container>
-        <h1>Top Músicas</h1>
-          <v-card class="mx-auto" color="surface-light" max-width="400">
-            <v-card-text>
-              <v-text-field
-                :loading="loading"
-                append-inner-icon="mdi-magnify"
-                density="compact"
-                label="Search templates"
-                variant="solo"
-                hide-details
-                single-line
-                @click:append-inner="onClick"
-              ></v-text-field>
-            </v-card-text>
-          </v-card>
+        <h1 style="text-align: center;">Top Músicas</h1>
+        <v-card class="mt-12 mx-auto" color="surface-light" max-width="400">
+          <v-card-text>
+            <v-text-field
+              :loading="loading"
+              append-inner-icon="mdi-magnify"
+              density="compact"
+              label="Search Musics"
+              variant="solo"
+              hide-details
+              single-line
+              @click:append-inner="onClick"
+            ></v-text-field>
+          </v-card-text>
+        </v-card>
       </v-container>
+
+      <v-col cols="3" class="mx-auto">
+        <v-card color="#1F7087">
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title class="text-h5"> Halcyon Days </v-card-title>
+
+              <v-card-subtitle>Ellie Goulding</v-card-subtitle>
+
+              <v-card-actions>
+                <v-btn class="ms-2" icon="mdi-play" variant="text"></v-btn>
+              </v-card-actions>
+            </div>
+
+            <v-avatar class="ma-3" rounded="0" size="125">
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
+              ></v-img>
+            </v-avatar>
+          </div>
+        </v-card>
+      </v-col>
+
+      <v-col cols="3" class="mx-auto">
+        <v-card color="#952175">
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title class="text-h5"> Halcyon Days </v-card-title>
+
+              <v-card-subtitle>Ellie Goulding</v-card-subtitle>
+
+              <v-card-actions>
+                <v-btn class="ms-2" icon="mdi-play" variant="text"></v-btn>
+              </v-card-actions>
+            </div>
+
+            <v-avatar class="ma-3" rounded="0" size="125">
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+              ></v-img>
+            </v-avatar>
+          </div>
+        </v-card>
+      </v-col>
+
     </v-main>
   </v-app>
 </template>
